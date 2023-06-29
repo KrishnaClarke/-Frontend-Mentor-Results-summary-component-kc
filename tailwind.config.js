6e009d37a3ed816c33e07609c2c6d5813972ae5a
Krishna Clarke <126019343+KrishnaClarke@users.noreply.github.com>
2023-06-29 09:38:51 -0400

    chandes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ["./*.html"],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
+
